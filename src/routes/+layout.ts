a305395e52c021740e410ba54e36897a823b63ab
horlakz <officialhorlakz@gmail.com>
2023-04-21 16:23:48 +0100

    fix(src/routes/): removes logo import from static file

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,5 @@
 import type { LayoutLoad } from "./$types";
 
-import logo from "../../static/favicon.png";
-
 export const load = (async () => {
-  return { title: "Energaan", logo: logo };
+  return { title: "Energaan", logo: "" };
 }) satisfies LayoutLoad;
