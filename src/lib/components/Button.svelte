de8bba65222b31077392d245b690dd1fb39fd3c6
horlakz <officialhorlakz@gmail.com>
2023-05-07 17:16:38 +0100

    feat(src/lib/components/): adds dynamic background with variants to button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
   export let href: string | undefined = undefined;
+
+  const variants = {
+    primary: "bg-primary text-white",
+    danger: "bg-red-600 text-white",
+    blue: "bg-blue-600 text-white",
+  };
 </script>
 
 <svelte:element
   this={href ? "a" : "button"}
   {href}
-  class="bg-primary text-white px-3.5 py-2 text-md rounded-lg cursor-pointer"
+  class={`${variants["primary"]} hover:bg-opacity-80 px-3.5 py-1.5 text-md rounded-lg cursor-pointer default-transition`}
   on:click
 >
   <slot />
