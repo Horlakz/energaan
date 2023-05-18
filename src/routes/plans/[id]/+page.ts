8a30a89dbbd5c0f5ddda6e25a0319632fea758a8
horlakz <officialhorlakz@gmail.com>
2023-05-18 17:10:37 +0100

    feat(src/routes/plans/[id]/): adds params to page client file

--- src/routes/plans/[id]/+page.ts
+++ src/routes/plans/[id]/+page.ts
@@ -1,5 +1,8 @@
 import type { PageLoad } from "./$types";
 
-export const load = (async () => {
-  return {};
+export const load = (async ({ params, url }) => {
+  return {
+    id: params.id,
+    // ref: url.searchParams.get("ref"),
+  };
 }) satisfies PageLoad;
