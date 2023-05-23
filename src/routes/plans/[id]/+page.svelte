f001fd48062f6d733ac24bf643b4a6b9946182f1
horlakz <officialhorlakz@gmail.com>
2023-05-23 16:00:59 +0100

    enhancement(src/routes/plans/\[id\]/): update page design and adds more dynamic data to page client file

--- src/routes/plans/[id]/+page.svelte
+++ src/routes/plans/[id]/+page.svelte
@@ -2,36 +2,38 @@
   import type { PageData } from "./$types";
 
   import Button from "$lib/components/Button.svelte";
-  import FormGroup from "$lib/components/FormGroup.svelte";
+  import GetQuoteForm from "$lib/components/GetQuoteForm.svelte";
+  import Modal from "$lib/components/Modal.svelte";
 
-  // export let data: PageData;
+  export let data: PageData;
+
+  let open = false;
 </script>
 
 <main class="sm:px-20 p-10">
   <section class="text-center mb-8 grid gap-2">
-    <img
-      src="https://dummyimage.com/200x150/09f/fff.png"
-      alt="Solar Streetlights Solution"
-      class="w-full h-48 object-cover"
-    />
-    <h3 class="text-3xl">Solar Streetlights Solution</h3>
-    <p>Contact the Seller for this plan quote</p>
+    <img src={data.image} alt={data.name} class="w-full h-48 object-cover" />
+    <h3 class="text-3xl">{data.name}</h3>
   </section>
 
-  <section
-    class="w-full flex md:flex-row flex-col justify-between items-center gap-10"
-  >
-    <form
-      class="w-full bg-primary bg-opacity-10 shadow-md p-4 border-2 rounded-md grid md:grid-cols-2 gap-4"
-    >
-      <FormGroup label="Full Name" />
-      <FormGroup label="Email" type="email" />
-      <FormGroup label="Phone" />
-      <FormGroup label="Country" />
+  <section class="flex flex-col center gap-6">
+    <p class="md:px-10">
+      {data.about}
+    </p>
 
-      <div class="md:col-span-2 items-center grid">
-        <Button>Submit Details</Button>
-      </div>
-    </form>
+    {#if data.features.length !== 0}
+      <ul class="w-full grid justify-start">
+        <h3 class="font-semibold text-lg underline">Key Features</h3>
+        {#each data.features as item}
+          <li class="list-disc m-2">{item}</li>
+        {/each}
+      </ul>
+    {/if}
+
+    <Button on:click={() => (open = true)}>Get Quote</Button>
   </section>
 </main>
+
+<Modal bind:open>
+  <GetQuoteForm />
+</Modal>
