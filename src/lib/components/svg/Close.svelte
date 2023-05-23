d84451f8d9d6ee0f175449b50f78c138a872a209
horlakz <officialhorlakz@gmail.com>
2023-05-23 15:56:21 +0100

    feat(src/lib/components/svg/): adds file for close svg

---
+++ src/lib/components/svg/Close.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let size: "xs" | "sm" | "md" = "md";
+
+  const svgSizes = {
+    xs: "w-3 h-3",
+    sm: "w-3.5 h-3.5",
+    md: "w-5 h-5",
+  };
+</script>
+
+<svg
+  class={svgSizes[size]}
+  fill="currentColor"
+  viewBox="0 0 20 20"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    fill-rule="evenodd"
+    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+    clip-rule="evenodd"
+  />
+</svg>
