0d56a9fe47d688857d27e46b658b8961074bd2f1
horlakz <officialhorlakz@gmail.com>
2023-05-23 15:58:21 +0100

    feat(src/lib/components/): creates component file for GetQuoteForm

---
+++ src/lib/components/GetQuoteForm.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Button from "./Button.svelte";
+  import FormGroup from "./FormGroup.svelte";
+</script>
+
+<div class="w-full bg-white shadow-md p-4 border-2 rounded-md">
+  <p class="text-sm text-gray-800 text-center py-4">
+    Contact the buyer to get a quote of this product
+  </p>
+  <form class="w-full grid md:grid-cols-2 gap-4">
+    <FormGroup label="Full Name" />
+    <FormGroup label="Email" type="email" />
+    <FormGroup label="Phone" />
+    <FormGroup label="Country" />
+
+    <div class="md:col-span-2 items-center grid">
+      <Button>Submit Details</Button>
+    </div>
+  </form>
+</div>
