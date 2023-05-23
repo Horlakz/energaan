7c0a53793723ed6d536362554a6ec923e082a6f3
horlakz <officialhorlakz@gmail.com>
2023-05-23 17:37:00 +0100

    feat(src/routes/gallery/): creates gallery page and designs it

---
+++ src/routes/gallery/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<main class="mb-10">
+  <h3 class="text-2xl mb-4 text-center font-semibold underline">Gallery</h3>
+
+  <section
+    class="sm:px-20 px-8 sm:py-10 w-full grid sm:grid-cols-3 md:grid-cols-4 gap-2 md:gap-6 h-full"
+  >
+    {#each data.images as item}
+      <div
+        class="border-2 border-primary shadow-md rounded-3xl overflow-hidden"
+      >
+        <img src={item.url} alt={item.name} class="w-full h-80" />
+        <span
+          class="text-lg font-medium px-4 w-full flex center bg-primary bg-opacity-20"
+          >{item.name}</span
+        >
+      </div>
+    {/each}
+  </section>
+</main>
---
+++ src/routes/gallery/+page.ts
@@ -0,0 +1,56 @@
+import type { PageLoad } from "./$types";
+
+export const load = (async () => {
+  return {
+    images: [
+      {
+        url: "https://dummyimage.com/200x150/09f/fff.png",
+        name: "Lorem Ipsum",
+      },
+      {
+        url: "https://dummyimage.com/200x150/09f/fff.png",
+        name: "Lorem Ipsum",
+      },
+      {
+        url: "https://dummyimage.com/200x150/09f/fff.png",
+        name: "Lorem Ipsum",
+      },
+      {
+        url: "https://dummyimage.com/200x150/09f/fff.png",
+        name: "Lorem Ipsum",
+      },
+      {
+        url: "https://dummyimage.com/200x150/09f/fff.png",
+        name: "Lorem Ipsum",
+      },
+      {
+        url: "https://dummyimage.com/200x150/09f/fff.png",
+        name: "Lorem Ipsum",
+      },
+      {
+        url: "https://dummyimage.com/200x150/09f/fff.png",
+        name: "Lorem Ipsum",
+      },
+      {
+        url: "https://dummyimage.com/200x150/09f/fff.png",
+        name: "Lorem Ipsum",
+      },
+      {
+        url: "https://dummyimage.com/200x150/09f/fff.png",
+        name: "Lorem Ipsum",
+      },
+      {
+        url: "https://dummyimage.com/200x150/09f/fff.png",
+        name: "Lorem Ipsum",
+      },
+      {
+        url: "https://dummyimage.com/200x150/09f/fff.png",
+        name: "Lorem Ipsum",
+      },
+      {
+        url: "https://dummyimage.com/200x150/09f/fff.png",
+        name: "Lorem Ipsum",
+      },
+    ],
+  };
+}) satisfies PageLoad;
