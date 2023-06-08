d45cef6362e35178dd463bc31a4ddf1c76285676
horlakz <officialhorlakz@gmail.com>
2023-06-08 04:17:32 +0100

    feat(src/lib/components/): adds props and mutation function to submit form to api

--- src/lib/components/GetQuoteForm.svelte
+++ src/lib/components/GetQuoteForm.svelte
@@ -1,6 +1,43 @@
-<script>
+<script lang="ts">
+  import { createMutation } from "@tanstack/svelte-query";
+
   import Button from "./Button.svelte";
   import FormGroup from "./FormGroup.svelte";
+  import Quote from "$lib/services/quote";
+
+  export let serviceId: string, serviceType: string;
+
+  let fullName = "",
+    email = "",
+    phone = "",
+    country = "";
+
+  $: formData = {
+    fullName,
+    email,
+    phone,
+    country,
+    serviceId,
+    serviceType,
+  };
+  const quote = new Quote();
+
+  const mutation = createMutation(async () => await quote.create(formData), {
+    onSuccess: () => {
+      console.log("success");
+
+      // clear form
+      fullName = "";
+      email = "";
+      phone = "";
+      country = "";
+    },
+  });
+
+  function handleSubmit(e: Event) {
+    e.preventDefault();
+    $mutation.mutate();
+  }
 </script>
 
 <div class="w-full bg-white shadow-md p-4 border-2 rounded-md">
@@ -8,13 +45,15 @@
     Contact the buyer to get a quote of this product
   </p>
   <form class="w-full grid md:grid-cols-2 gap-4">
-    <FormGroup label="Full Name" />
-    <FormGroup label="Email" type="email" />
-    <FormGroup label="Phone" />
-    <FormGroup label="Country" />
+    <FormGroup label="Full Name" bind:value={fullName} />
+    <FormGroup label="Email" type="email" bind:value={email} />
+    <FormGroup label="Phone" bind:value={phone} />
+    <FormGroup label="Country" bind:value={country} />
 
     <div class="md:col-span-2 items-center grid">
-      <Button>Submit Details</Button>
+      <Button isLoading={$mutation.isLoading} on:click={handleSubmit}>
+        Submit Details
+      </Button>
     </div>
   </form>
 </div>
