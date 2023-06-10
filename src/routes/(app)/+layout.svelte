e0f7d36fec85e0c289a35c1440854d2ae0c4bf14
horlakz <officialhorlakz@gmail.com>
2023-06-10 09:00:58 +0100

    feat(src/routes/): creates non-route folder and moves all app dir inside

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { LayoutData } from "./$types";
+
+  import Footer from "$lib/components/baselayout/Footer.svelte";
+  import Header from "$lib/components/baselayout/Header.svelte";
+
+  export let data: LayoutData;
+</script>
+
+<Header title={data.title} />
+<slot />
+<Footer />
