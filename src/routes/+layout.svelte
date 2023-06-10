b67975c93d98a1bfb6ca845312b6c19d791ee06d
horlakz <officialhorlakz@gmail.com>
2023-06-10 09:06:52 +0100

    feat(src/routes/): create base layout for layout functions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
-  import type { LayoutData } from "./$types";
   import { browser } from "$app/environment";
   import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
 
   import "styles/global.scss";
-  import Header from "$lib/components/baselayout/Header.svelte";
-  import Footer from "$lib/components/baselayout/Footer.svelte";
-  import logo from "$lib/assets/logo.png";
-
-  export let data: LayoutData;
 
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -23,14 +17,12 @@
   <meta name="description" content="a website for properties and hosting" />
   <meta
     name="keywords"
-    content="properties, housing, property and hosting, airbnb, book an apartment"
+    content="solar, panels, solar panels, inverter, battery"
   />
-  <link rel="shortcut icon" href={logo} />
-  <title>{data.title}</title>
+  <link rel="shortcut icon" href="/favicon.png" />
+  <title>Energaan</title>
 </svelte:head>
 
 <QueryClientProvider client={queryClient}>
-  <Header title={data.title} />
   <slot />
-  <Footer />
 </QueryClientProvider>
