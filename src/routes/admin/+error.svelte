07448b4db190aeb3ebc27295babf8d370138d099
horlakz <officialhorlakz@gmail.com>
2023-06-11 18:35:26 +0100

    enhancement(src/routes/admin/): updates page design

--- src/routes/admin/+error.svelte
+++ src/routes/admin/+error.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
   import { page } from "$app/stores";
+
+  import Button from "$lib/components/Button.svelte";
 </script>
 
-<h1>{$page.status}: {$page.error?.message}</h1>
+<main class="flex flex-col justify-center items-center p-10">
+  {#if $page.status !== 404}
+    <p>
+      Status: {$page.status}
+    </p>
+    <p>
+      Message: {$page.error?.message}
+    </p>
+  {:else}
+    <p class="text-2xl">This page cannot be found on this website</p>
+    <Button href="/">Click to go Back Home</Button>
+  {/if}
+</main>
