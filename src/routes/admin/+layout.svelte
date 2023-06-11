c6b7630c5fa3bde560a7846455fa90004bf63cc5
horlakz <officialhorlakz@gmail.com>
2023-06-11 03:37:08 +0100

    enhancement(src/routes/admin/): refactors layout and adds newly created nav component

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
+  import Nav from "$lib/components/admin/Nav.svelte";
   import type { LayoutData } from "./$types";
 
   export let data: LayoutData;
 </script>
 
-<slot />
+<div class="w-full flex">
+  <div class="w-1/6">
+    <Nav />
+  </div>
+  <div class="w-5/6">
+    <slot />
+  </div>
+</div>
