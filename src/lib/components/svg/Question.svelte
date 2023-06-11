318b6afede9c18a21f2ee76b96ebe566971525a2
horlakz <officialhorlakz@gmail.com>
2023-06-11 03:28:12 +0100

    feat(src/lib/components/svg/): adds new svg components

---
+++ src/lib/components/svg/Question.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import classNames from "classnames";
+
+  export let customColor: string = "";
+</script>
+
+<span
+  class={classNames(
+    customColor ? customColor : "text-gray-300",
+    "font-extrabold text-lg mx-1"
+  )}>?</span
+>
