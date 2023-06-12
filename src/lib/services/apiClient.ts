d362e78b27d3fe964bc9f0a3bae3c115fead199d
horlakz <officialhorlakz@gmail.com>
2023-06-12 13:21:46 +0100

    feat(src/lib/services/): adds interceptors to api client for authentation and errors

--- src/lib/services/apiClient.ts
+++ src/lib/services/apiClient.ts
@@ -7,3 +7,19 @@ client.defaults.baseURL = PUBLIC_API_URL;
 client.defaults.xsrfHeaderName = "X-CSRFToken";
 client.defaults.responseEncoding = "utf8";
 client.defaults.headers["common"]["Content-Type"] = "application/json";
+client.interceptors.request.use(function (config) {
+  config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  return config;
+});
+client.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 403 || error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
