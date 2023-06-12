6892756057d777cf3e48f4d8c7a2881c90cbade0
horlakz <officialhorlakz@gmail.com>
2023-06-12 07:04:51 +0100

    feat(src/routes/admin): adds fixed and scroll value to overflow to make contents viewable

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -5,11 +5,11 @@
   export let data: LayoutData;
 </script>
 
-<div class="w-full flex">
+<div class="w-full flex h-screen">
   <div class="w-1/6">
     <Nav />
   </div>
-  <div class="w-5/6">
+  <div class="w-5/6 overflow-scroll">
     <slot />
   </div>
 </div>
