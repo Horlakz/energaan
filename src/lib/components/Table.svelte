82e2794cad832374e43323adcb7f64a89c7fe488
horlakz <officialhorlakz@gmail.com>
2023-06-16 21:37:51 +0100

    feat(src/lib/components/): updates class styles

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { twMerge } from "tailwind-merge";
+
   import Button from "./Button.svelte";
 
   export let tableHeaders: TableHeadersT[],
@@ -22,12 +24,14 @@
 
 <div class="relative overflow-x-auto rounded-lg">
   <table class="w-full text-sm text-left text-gray-500">
-    <thead class="text-primary font-extrabold capitalize">
+    <thead
+      class="text-primary font-extrabold capitalize border-b border-primary border-opacity-30"
+    >
       <tr>
         {#each tableHeaders as header}
           <th
             scope="col"
-            class={`px-4 py-2 font-medium ${header?.class || ""}`}
+            class={twMerge("px-4 py-2 font-medium", header?.class)}
             colSpan={header?.colSpan || 1}
           >
             {header.title}
