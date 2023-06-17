4d6d45180c02d009c3d5daae6e502e806156bc66
horlakz <officialhorlakz@gmail.com>
2023-06-17 11:56:00 +0100

    feat(src/routes/admin/services/): updates products card

--- src/routes/admin/services/+page.svelte
+++ src/routes/admin/services/+page.svelte
@@ -2,6 +2,8 @@
   import { Plus } from "svelte-heros-v2";
   import { writable } from "svelte/store";
   import type { PageData } from "./$types";
+  import { invalidateAll } from "$app/navigation";
+  import { createMutation, createQuery } from "@tanstack/svelte-query";
 
   import Button from "$lib/components/Button.svelte";
   import FormGroup from "$lib/components/FormGroup.svelte";
@@ -9,12 +11,20 @@
   import CreateServiceForm from "$lib/components/admin/CreateServiceForm.svelte";
   import TabItem from "$lib/components/tabs/TabItem.svelte";
   import Tabs from "$lib/components/tabs/Tabs.svelte";
+  import Categories from "$lib/components/admin/services/Categories.svelte";
+  import Category from "$lib/services/services/category";
+  import Product from "$lib/services/services/product";
+  import Card from "$lib/components/Card.svelte";
 
-  export let data: PageData;
+  export let data: PageData,
+    currentCategoryID = "";
 
+  const categorySvc = new Category();
+  const productSvc = new Product();
   let tabTitle = "",
     plan = false,
-    product = false;
+    product = false,
+    category = false;
 
   let title = "",
     description = "",
@@ -22,6 +32,19 @@
     images = null;
   const features = writable<string[]>([]);
   let featureValue = "";
+
+  $: categoryData = { name: title };
+
+  const createCategory = createMutation(
+    async () => await categorySvc.create(categoryData.name),
+    {
+      onSuccess: () => {
+        title = "";
+        category = false;
+        invalidateAll();
+      },
+    }
+  );
 </script>
 
 <main>
@@ -32,25 +55,59 @@
       iconPosition="left"
       variant="primary"
       on:click={() => {
-        if (tabTitle === "Plans") plan = true;
-        else product = true;
+        switch (tabTitle) {
+          case "Plans":
+            plan = true;
+            break;
+          case "Products":
+            product = true;
+            break;
+          case "Categories":
+            category = true;
+            break;
+          default:
+            plan = true;
+        }
       }}
     >
-      Add New {tabTitle.replace(/s$/, "")}
+      Add New {tabTitle != "Categories" ? tabTitle.replace(/s$/, "") : tabTitle}
     </Button>
   </div>
 
   <Tabs>
     <TabItem open title="Plans" on:currentTab={(e) => (tabTitle = e.detail)}>
-      <p class="text-lg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </p>
+      <div class="grid grid-cols-4 gap-4">
+        {#each [1, 2, 3, 4, 5, 6, 3, 4] as plan}
+          <Card
+            linkText="Go to plan"
+            buttonText="Delete Plan"
+            buttonVariant="danger"
+          />
+        {/each}
+      </div>
     </TabItem>
-    <TabItem open title="Products" on:currentTab={(e) => (tabTitle = e.detail)}>
-      <p class="text-lg">
-        Lorem ipsum dolor veniam esse tempore, debitis molestiae consequatur
-        magnam quod repellat sapiente.
-      </p>
+    <TabItem title="Products" on:currentTab={(e) => (tabTitle = e.detail)}>
+      <div class="grid grid-cols-4 gap-4">
+        {#each [1, 2, 3, 4, 5, 6, 3] as product}
+          <Card
+            linkText="Go to product"
+            buttonText="Delete product"
+            buttonVariant="danger"
+          />
+        {/each}
+      </div>
+    </TabItem>
+    <TabItem title="Categories" on:currentTab={(e) => (tabTitle = e.detail)}>
+      <Categories
+        data={data.categories.map((cat) => {
+          currentCategoryID = cat.uuid;
+          return {
+            name: cat.category.name,
+            shortname: cat.category.slug,
+            created_by: cat.createdByDetails,
+          };
+        })}
+      />
     </TabItem>
   </Tabs>
 </main>
@@ -69,4 +126,19 @@
     {categoryId}
     {images}
   />
+</Modal>
+
+<Modal bind:open={category}>
+  <div class="bg-white p-4 grid center rounded-xl">
+    <h4 class="text-xl font-semibold">Create New Category</h4>
+    <form class="space-y-3" on:submit|preventDefault>
+      <FormGroup label="Title" bind:value={title} />
+      <Button
+        isLoading={$createCategory.isLoading}
+        on:click={() => $createCategory.mutate()}
+      >
+        Create
+      </Button>
+    </form>
+  </div>
 </Modal>
