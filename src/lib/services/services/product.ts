f03d2f6c210f5ff5b179934b3a391f40d7454710
horlakz <officialhorlakz@gmail.com>
2023-06-18 19:34:34 +0100

    fix(src/lib/services/services/): converts json data to form data

--- src/lib/services/services/product.ts
+++ src/lib/services/services/product.ts
@@ -4,16 +4,26 @@ interface ProductData {
   title: string;
   description: string;
   features: string[];
-  image: File;
+  image: File[];
   categoryId: string;
 }
 
 export default class Product {
   create(data: ProductData) {
+    const newProductData = new FormData();
+    newProductData.append("title", data.title);
+    newProductData.append("description", data.description);
+    newProductData.append("categoryId", data.categoryId);
+    for (let i = 0; i < data.features.length; i++) {
+      newProductData.append("features", data.features[i]);
+    }
+    for (let i = 0; i < data.image.length; i++) {
+      newProductData.append("image", data.image[i]);
+    }
     return client({
       method: "POST",
       url: "/products",
-      data,
+      data: newProductData,
       headers: { "Content-Type": "multipart/form-data" },
     });
   }
